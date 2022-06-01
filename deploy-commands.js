b09766fabe3d89f8cf4fb321e2d2f758035590d7
ryry08 <rjpreme2@gmail.com>
2022-06-01 16:32:51 +0100

    oioi

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
+const ytdl = require("ytdl-core");
 
 const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
